b5c1ab7177535271241c6d76dbc4633ae4c5d673
Stefan Kopieczek <stefankopieczek@gmail.com>
2014-05-25 08:38:15 +0100

    Add Request and Response types to messages.go.
    Remove stand-alone RequestLine and StatusLine types, and add Request and
    Response types.

--- messages.go
+++ messages.go
@@ -1,39 +1,87 @@
 package gossip
 
-import "fmt"
+import (
+    "bytes"
+    "fmt"
+)
 
 type Method string
 
 const (
-    INVITE  Method = "INVITE"
-    ACK     Method = "ACK"
-    CANCEL  Method = "CANCEL"
-    BYE     Method = "BYE"
-    OPTIONS Method = "OPTIONS"
+    INVITE   Method = "INVITE"
+    ACK      Method = "ACK"
+    CANCEL   Method = "CANCEL"
+    BYE      Method = "BYE"
+    REGISTER Method = "REGISTER"
+    OPTIONS  Method = "OPTIONS"
 )
 
 type SipMessage interface {
     String()
 }
 
-// A RequestLine is required at the start of every SIP request - RFC 3261 7.1.
-type RequestLine struct {
+type Request struct {
     method Method
     uri SipUri
     sipVersion string
+    headers []SipHeader
+    body *string
 }
-func (requestLine *RequestLine) String() (string) {
-    return fmt.Sprintf("%s %s %s", (string)(requestLine.method),
-        requestLine.uri.String(), requestLine.sipVersion)
+func (request *Request) String() (string) {
+    var buffer bytes.Buffer
+
+    // Every SIP request starts with a Request Line - RFC 2361 7.1.
+    buffer.WriteString(fmt.Sprintf("%s %s %s\r\n",
+        (string)(request.method),
+        request.uri.String(),
+        request.sipVersion))
+
+    // Construct each header in turn and add it to the message.
+    for idx, header := range(request.headers) {
+        buffer.WriteString(header.String())
+
+        if (idx < len(request.headers)) {
+            buffer.WriteString("\r\n")
+        }
+    }
+
+    // If the request has a message body, add it.
+    if (request.body != nil) {
+        buffer.WriteString(*request.body)
+    }
+
+    return buffer.String()
 }
 
-// A StatusLine is required at the start of every SIP response - RFC 3261 7.2.
-type StatusLine struct {
+type Response struct {
     sipVersion string
     statusCode uint8
     reason string
+    headers []SipHeader
+    body *string
 }
-func (statusLine *StatusLine) String() (string) {
-    return fmt.Sprintf("%s %d %s", statusLine.sipVersion,
-        statusLine.statusCode, statusLine.reason)
+func (response *Response) String() (string) {
+    var buffer bytes.Buffer
+
+    // Every SIP response starts with a Status Line - RFC 2361 7.2.
+    buffer.WriteString(fmt.Sprintf("%s %d %s",
+        response.sipVersion,
+        response.statusCode,
+        response.reason))
+
+    // Construct each header in turn and add it to the message.
+    for idx, header := range(response.headers) {
+        buffer.WriteString(header.String())
+
+        if (idx < len(response.headers)) {
+            buffer.WriteString("\r\n")
+        }
+    }
+
+    // If the request has a message body, add it.
+    if (response.body != nil) {
+        buffer.WriteString(*response.body)
+    }
+
+    return buffer.String()
 }
