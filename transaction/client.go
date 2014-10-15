741fa5937e93b278b9d4bc13d84cc935d82cc40f
Ryan Norris <rynorris@gmail.com>
2014-10-15 23:30:16 +0100

    Added support for non-INVITE client transaction

--- transaction/client.go
+++ transaction/client.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/discoviking/fsm"
+	"github.com/stefankopieczek/gossip/base"
 	"github.com/stefankopieczek/gossip/log"
 )
 
@@ -29,7 +30,16 @@ const (
 	client_input_transport_err
 )
 
+// Initialises the correct kind of FSM based on request method.
 func (tx *ClientTransaction) initFSM() {
+	if tx.origin.Method == base.INVITE {
+		tx.initInviteFSM()
+	} else {
+		tx.initNonInviteFSM()
+	}
+}
+
+func (tx *ClientTransaction) initInviteFSM() {
 	// Define Actions
 
 	// Resend the request.
@@ -139,7 +149,121 @@ func (tx *ClientTransaction) initFSM() {
 	)
 
 	if err != nil {
-		log.Severe("Failure to define client transaction fsm: %s", err.Error())
+		log.Severe("Failure to define INVITE client transaction fsm: %s", err.Error())
+	}
+
+	tx.fsm = fsm
+}
+
+func (tx *ClientTransaction) initNonInviteFSM() {
+	// Define Actions
+
+	// Resend the request.
+	act_resend := func() fsm.Input {
+		tx.timer_a_time *= 2
+		// For non-INVITE, cap timer A at T2 seconds.
+		if tx.timer_a_time > T2 {
+			tx.timer_a_time = T2
+		}
+
+		tx.timer_a.Reset(tx.timer_a_time)
+		tx.resend()
+		return fsm.NO_INPUT
+	}
+
+	// Just pass up the latest response.
+	act_passup := func() fsm.Input {
+		tx.passUp()
+		return fsm.NO_INPUT
+	}
+
+	// Handle a final response.
+	act_final := func() fsm.Input {
+		tx.passUp()
+		if tx.timer_d != nil {
+			tx.timer_d.Stop()
+		}
+		tx.timer_d = time.AfterFunc(tx.timer_d_time, func() {
+			tx.fsm.Spin(client_input_timer_d)
+		})
+		return fsm.NO_INPUT
+	}
+
+	// Send up transport failure error.
+	act_trans_err := func() fsm.Input {
+		tx.transportError()
+		return fsm.NO_INPUT
+	}
+
+	// Send up timeout error.
+	act_timeout := func() fsm.Input {
+		tx.timeoutError()
+		return fsm.NO_INPUT
+	}
+
+	// Define States
+
+	// "Trying"
+	client_state_def_calling := fsm.State{
+		Index: client_state_calling,
+		Outcomes: map[fsm.Input]fsm.Outcome{
+			client_input_1xx:           {client_state_proceeding, act_passup},
+			client_input_2xx:           {client_state_completed, act_final},
+			client_input_300_plus:      {client_state_completed, act_final},
+			client_input_timer_a:       {client_state_calling, act_resend},
+			client_input_timer_b:       {client_state_terminated, act_timeout},
+			client_input_transport_err: {client_state_terminated, act_trans_err},
+		},
+	}
+
+	// Proceeding
+	client_state_def_proceeding := fsm.State{
+		Index: client_state_proceeding,
+		Outcomes: map[fsm.Input]fsm.Outcome{
+			client_input_1xx:           {client_state_proceeding, act_passup},
+			client_input_2xx:           {client_state_completed, act_final},
+			client_input_300_plus:      {client_state_completed, act_final},
+			client_input_timer_a:       {client_state_proceeding, act_resend},
+			client_input_timer_b:       {client_state_terminated, act_timeout},
+			client_input_transport_err: {client_state_terminated, act_trans_err},
+		},
+	}
+
+	// Completed
+	client_state_def_completed := fsm.State{
+		Index: client_state_completed,
+		Outcomes: map[fsm.Input]fsm.Outcome{
+			client_input_1xx:      {client_state_completed, fsm.NO_ACTION},
+			client_input_2xx:      {client_state_completed, fsm.NO_ACTION},
+			client_input_300_plus: {client_state_completed, fsm.NO_ACTION},
+			client_input_timer_d:  {client_state_terminated, fsm.NO_ACTION},
+			client_input_timer_a:  {client_state_completed, fsm.NO_ACTION},
+			client_input_timer_b:  {client_state_completed, fsm.NO_ACTION},
+		},
+	}
+
+	// Terminated
+	client_state_def_terminated := fsm.State{
+		Index: client_state_terminated,
+		Outcomes: map[fsm.Input]fsm.Outcome{
+			client_input_1xx:      {client_state_terminated, fsm.NO_ACTION},
+			client_input_2xx:      {client_state_terminated, fsm.NO_ACTION},
+			client_input_300_plus: {client_state_terminated, fsm.NO_ACTION},
+			client_input_timer_a:  {client_state_terminated, fsm.NO_ACTION},
+			client_input_timer_b:  {client_state_terminated, fsm.NO_ACTION},
+			client_input_timer_d:  {client_state_terminated, fsm.NO_ACTION},
+		},
+	}
+
+	fsm, err := fsm.Define(
+		client_state_def_calling,
+		client_state_def_proceeding,
+		client_state_def_completed,
+		client_state_def_terminated,
+	)
+
+	if err != nil {
+		log.Severe("Failure to define INVITE client transaction fsm: %s", err.Error())
 	}
 
 	tx.fsm = fsm
--- transaction/transaction.go
+++ transaction/transaction.go
@@ -14,6 +14,7 @@ import (
 
 const (
 	T1 = 500 * time.Millisecond
+	T2 = 4 * time.Second
 )
 
 type Transaction interface {
