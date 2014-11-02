eeec8607011e192c2218adfcbdf108e01901dbb1
Stefan <stefankopieczek@gmail.com>
2014-11-02 17:47:47 +0000

    Interfaced transaction manager with transport manager

--- transaction/manager.go
+++ transaction/manager.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/stefankopieczek/gossip/base"
+	"github.com/stefankopieczek/gossip/transport"
 )
 
 var (
@@ -13,7 +14,8 @@ var (
 )
 
 type Manager struct {
-	txs map[key]Transaction
+	txs       map[key]Transaction
+	transport *transport.Manager
 }
 
 // Transactions are identified by the branch parameter in the top Via header, and the method. (RFC 3261 17.1.3)
@@ -22,19 +24,60 @@ type key struct {
 	method string
 }
 
+func NewManager(trans, addr string) (*Manager, error) {
+	t, err := transport.NewManager(trans, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	mng := &Manager{
+		txs:       map[key]Transaction{},
+		transport: t,
+	}
+
+	// Spin up a goroutine to pull messages up from the depths.
+	go func() {
+		c := mng.transport.GetChannel()
+		for msg := range c {
+			mng.Handle(msg)
+		}
+	}()
+
+	return mng, nil
+}
+
 func (mng *Manager) putTx(tx Transaction) {
-	key := key{"", string(tx.Origin().Method)}
+	viaHeaders := tx.Origin().Headers("Via")
+	via := viaHeaders[0].(base.ViaHeader)
+	branch := *via[0].Params["branch"]
+	// TODO: Safety
+
+	key := key{branch, string(tx.Origin().Method)}
 	mng.txs[key] = tx
 }
 
 func (mng *Manager) getTx(s base.SipMessage) (Transaction, bool) {
-	key := key{"", ""}
+	viaHeaders := s.Headers("Via")
+	via, _ := viaHeaders[0].(base.ViaHeader)
+	branch := *via[0].Params["branch"]
+	// TODO: Safety
+
+	var method string
+	switch s := s.(type) {
+	case *base.Request:
+		method = string(s.Method)
+	case *base.Response:
+		cseq, _ := s.Headers("CSeq")[0].(*base.CSeq)
+		method = string(cseq.MethodName)
+	}
+
+	key := key{branch, method}
 	tx, ok := mng.txs[key]
 
 	return tx, ok
 }
 
-func (mng *Manager) Handle(msg base.SipMessage, src, transport string) {
+func (mng *Manager) Handle(msg base.SipMessage) {
 	switch m := msg.(type) {
 	case *base.Request:
 		// TODO: Create a new server transaction.
@@ -46,24 +89,29 @@ func (mng *Manager) Handle(msg base.SipMessage, src, transport string) {
 }
 
 // Create Client transaction.
-func (mng *Manager) Send(r *base.Request, dest, transport string) {
+func (mng *Manager) Send(r *base.Request, dest string) (<-chan *base.Response, error) {
 	tx := &ClientTransaction{}
 	tx.origin = r
 	tx.dest = dest
-	tx.transport = transport
 
 	tx.initFSM()
 
-	tx.tu = make(chan *base.Response, 3)
+	respChan := make(chan *base.Response, 3)
+	tx.tu = (chan<- *base.Response)(respChan)
 	tx.tu_err = make(chan error, 1)
 
 	tx.timer_a_time = T1
 	tx.timer_a = time.NewTimer(tx.timer_a_time)
 	tx.timer_b = time.NewTimer(64 * T1)
 
-	// TODO: Send the request.
+	err := mng.transport.Send(dest, r)
+	if err != nil {
+		tx.fsm.Spin(client_input_transport_err)
+	}
 
 	mng.putTx(tx)
+
+	return (<-chan *base.Response)(respChan), err
 }
 
 // Give a received response to the correct transaction.
