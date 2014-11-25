57bb0a47324bb8e57616468649721089445d3307
Stefan Kopieczek <stefankopieczek@gmail.com>
2014-11-25 17:17:37 +0000

    Tidy up transaction tests

--- transaction/client_test.go
+++ transaction/client_test.go
@@ -18,51 +18,77 @@ var c_CLIENT string = "localhost:5061"
 
 var testLog *log.Logger = log.New(os.Stderr, ">>> ", 0)
 
+var c_INVITE *base.Request
+var c_OK *base.Response
+
 func TestAAAASetup(t *testing.T) {
 	log.SetDefaultLogLevel(log.WARN)
 	testLog.Level = log.INFO
-}
+	var err error
 
-func TestSendInviteUDP(t *testing.T) {
-	invite, err := request([]string{
-		"INVITE sip:joe@bloggs.com SIP/2.0",
-		"Via: SIP/2.0/UDP " + c_CLIENT + ";branch=z9hG4bK776asdhds",
-		"",
-		"",
-	})
-	assertNoError(t, err)
-
-	test := transactionTest{actions: []action{
-		&clientSend{invite},
-		&serverRecv{invite},
-	}}
-	test.Execute(t)
-}
-
-func TestReceiveOKUDP(t *testing.T) {
-	invite, err := request([]string{
+	c_INVITE, err = request([]string{
 		"INVITE sip:joe@bloggs.com SIP/2.0",
 		"CSeq: 1 INVITE",
 		"Via: SIP/2.0/UDP " + c_CLIENT + ";branch=z9hG4bK776asdhds",
 		"",
 		"",
 	})
-	assertNoError(t, err)
+	if err != nil {
+		panic("Unexpected error in setup: " + err.Error())
+	}
 
-	ok, err := response([]string{
+	c_OK, err = response([]string{
 		"SIP/2.0 200 OK",
 		"CSeq: 1 INVITE",
 		"Via: SIP/2.0/UDP " + c_SERVER + ";branch=z9hG4bK776asdhds",
 		"",
 		"",
 	})
+	if err != nil {
+		panic("Unexpected error in setup: " + err.Error())
+	}
+}
+
+func TestPortInUse(t *testing.T) {
+	_, err := NewManager("udp", c_CLIENT)
+	// defer client1.Stop()
 	assertNoError(t, err)
 
+	_, err = NewManager("udp", c_CLIENT)
+	// defer client2.Stop
+	if err == nil {
+		t.Fatalf("Unexpected success when port was in use.")
+	}
+}
+
+func TestSendInviteUDP(t *testing.T) {
 	test := transactionTest{actions: []action{
-		&clientSend{invite},
-		&serverRecv{invite},
-		&serverSend{ok},
-		&clientRecv{ok},
+		&clientSend{c_INVITE},
+		&serverRecv{c_INVITE},
+	}}
+	test.Execute(t)
+}
+
+func TestReceiveOKUDP(t *testing.T) {
+	test := transactionTest{actions: []action{
+		&clientSend{c_INVITE},
+		&serverRecv{c_INVITE},
+		&serverSend{c_OK},
+		&clientRecv{c_OK},
+	}}
+	test.Execute(t)
+}
+
+func TestResendInviteUDP(t *testing.T) {
+	test := transactionTest{actions: []action{
+		&clientSend{c_INVITE},
+		&serverRecv{c_INVITE},
+		&serverTimedRecv{c_INVITE, time.Millisecond * 300, time.Millisecond * 600},
+		&serverTimedRecv{c_INVITE, time.Millisecond * 700, time.Millisecond * 1100},
+		&serverTimedRecv{c_INVITE, time.Millisecond * 1500, time.Millisecond * 2100},
+		&serverTimedRecv{c_INVITE, time.Millisecond * 3000, time.Millisecond * 4100},
+		&serverTimedRecv{c_INVITE, time.Millisecond * 6000, time.Millisecond * 8100},
+		&serverTimedRecv{c_INVITE, time.Millisecond * 12000, time.Millisecond * 16100},
 	}}
 	test.Execute(t)
 }
@@ -93,7 +119,7 @@ func (test *transactionTest) Execute(t *testing.T) {
 
 	for _, actn := range test.actions {
 		testLog.Debug("Performing action %v", actn)
-		actn.Act(test)
+		assertNoError(t, actn.Act(test))
 	}
 }
 
@@ -149,19 +175,53 @@ func (actn *serverRecv) Act(test *transactionTest) error {
 			return nil
 		}
 	case <-time.After(time.Second):
-		return fmt.Errorf("Timed out waiting for message on server")
+		return fmt.Errorf("Timed out waiting for message %s on server", actn.expected.Short())
+	}
+}
+
+type serverTimedRecv struct {
+	expected       base.SipMessage
+	deadPeriod     time.Duration
+	expectedPeriod time.Duration
+}
+
+func (actn *serverTimedRecv) Act(test *transactionTest) error {
+	// First wait for a duration of deadPeriod; we expect no messages in this time.
+	select {
+	case msg, ok := <-test.serverRecv:
+		if !ok {
+			return fmt.Errorf("Server receive channel prematurely closed")
+		} else {
+			return fmt.Errorf("Message %s arrived unexpectedly early", msg.Short())
+		}
+	case <-time.After(actn.deadPeriod):
+		break
+	}
+
+	// Now wait to receive the message, with a timeout of expectedPeriod.
+	select {
+	case msg, ok := <-test.serverRecv:
+		if !ok {
+			return fmt.Errorf("Server receive channel prematurely closed")
+		} else if msg.String() != actn.expected.String() {
+			return fmt.Errorf("Unexpected message arrived at server:\n%s", msg.String())
+		} else {
+			return nil
+		}
+	case <-time.After(actn.expectedPeriod):
+		return fmt.Errorf("Timed out waiting for message %s on server", actn.expected.Short())
 	}
 }
 
 func assert(t *testing.T, b bool, msg string) {
 	if !b {
-		t.Errorf(msg)
+		t.Fatalf(msg)
 	}
 }
 
 func assertNoError(t *testing.T, err error) {
 	if err != nil {
-		t.Errorf("Unexpected error: %s", err.Error())
+		t.Fatalf("Unexpected error: %s", err.Error())
 	}
 }
 
